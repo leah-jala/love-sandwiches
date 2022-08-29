bce1a087f3be9872ed6daabe789d451c5da3fc88
leah-jala <leahlafond@protonmail.com>
2022-08-29 15:54:14 +0000

    Add user request loop to repeat request for data until valid data is received.

--- run.py
+++ run.py
@@ -28,14 +28,20 @@ def get_sales_data():
     """
     Get sales data figures, with input from User
     """
-    print("Please enter sales data from the last Market.")
-    print("Data should include 6 numbers, separated by commas")   
-    print("Example: 10, 20, 30, 40, 50, 60")    
+    while True:
+        print("Please enter sales data from the last Market.")
+        print("Data should include 6 numbers, separated by commas")   
+        print("Example: 10, 20, 30, 40, 50, 60")    
 
-    data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -51,5 +57,7 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
+    return True
 
-get_sales_data()                                                                                                                                                                                                                                                                                                                                                                                                                                         
+data = get_sales_data()                                                                                                                                                                                                                                                                                                                                                                                                                                         
